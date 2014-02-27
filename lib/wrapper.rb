f9fe21af83aec7b37270371841ef8193e68b6c22
Eamonn Webster <eweb@qstream.com>
2014-02-27 14:53:56 +0000

    Wrap and then unwrap objects of type ActiveRecord::Base

---
+++ lib/wrapper.rb
@@ -0,0 +1,51 @@
+module Wrapper
+  def unwrap_arg(arg)
+    if arg.is_a?(Array) && arg[0] == 'active_record'
+      arg[1].constantize.find_by_id(arg[2])
+    elsif arg.is_a?(Array)
+      unwrap_array(arg)
+    elsif arg.is_a?(Hash)
+      unwrap_hash(arg)
+    else
+      arg
+    end
+  end
+
+  def unwrap_array(args)
+    args.map do |arg|
+      unwrap_arg(arg)
+    end
+  end
+
+  def unwrap_hash(args)
+    Hash[args.map do |k, v|
+      [unwrap_arg(k), unwrap_arg(v)]
+    end]
+  end
+
+  def wrap_arg(arg)
+    if arg.is_a?(ActiveRecord::Base)
+      ['active_record', arg.class.name, arg.id]
+    elsif arg.is_a?(Array)
+      wrap_array(arg)
+    elsif arg.is_a?(Hash)
+      wrap_hash(arg)
+    else
+      arg
+    end
+  end
+
+  def wrap_array(args)
+    args.map do |arg|
+      wrap_arg(arg)
+    end
+  end
+
+  def wrap_hash(args)
+    Hash[args.map do |k, v|
+      [wrap_arg(k), wrap_arg(v)]
+    end]
+  end
+
+end
+
